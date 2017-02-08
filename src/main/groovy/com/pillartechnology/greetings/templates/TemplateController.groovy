1ee575e283705d1658fc0d001e0b642c0e5550f4
Nate Gordon <nlgordon@gmail.com>
2017-02-07 23:11:44 -0600

    Template controller now gets data from the template service

--- src/main/groovy/com/pillartechnology/greetings/templates/TemplateController.groovy
+++ src/main/groovy/com/pillartechnology/greetings/templates/TemplateController.groovy
@@ -2,8 +2,10 @@ package com.pillartechnology.greetings.templates
 
 class TemplateController {
 
+    TemplateService templateService
+
     TemplateResponse getTemplate(String name) {
-        return new TemplateResponse(template: "test template")
+        return new TemplateResponse(template: templateService.getTemplate(name))
     }
 
     static class TemplateResponse {
--- src/test/groovy/com/pillartechnology/greetings/templates/TemplateControllerTest.groovy
+++ src/test/groovy/com/pillartechnology/greetings/templates/TemplateControllerTest.groovy
@@ -3,11 +3,16 @@ package com.pillartechnology.greetings.templates
 import spock.lang.Specification
 
 class TemplateControllerTest extends Specification {
+    TemplateController controller
 
-    def "allows asking for a template"() {
-        setup:
-        TemplateController controller = new TemplateController()
+    def setup() {
+        controller = new TemplateController()
+        controller.templateService = Mock(TemplateService) {
+            getTemplate("test") >> "test template"
+        }
+    }
 
+    def "allows asking for a template from the template service"() {
         when:
         TemplateController.TemplateResponse template = controller.getTemplate("test")
 

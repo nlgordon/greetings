faf105c90eec37a05405b2fd45594a640a28e4ac
Nate Gordon <nlgordon@gmail.com>
2017-02-23 23:27:58 -0600

    Adding actuator to my api

---
+++ src/integration/groovy/com/insanedev/greetings/ActuatorTest.groovy
@@ -0,0 +1,23 @@
+package com.insanedev.greetings
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ActuatorTest extends Specification {
+    @Autowired
+    TestRestTemplate restTemplate
+
+    def "actuator health responds"() {
+        when:
+        ResponseEntity<String> response = restTemplate.exchange("/health", HttpMethod.GET, null, String)
+
+        then:
+        response.statusCode == HttpStatus.OK
+    }
+}

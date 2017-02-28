6fd1d6825eea4876841834bfbcc3610e7fe3e435
Nate Gordon <nlgordon@gmail.com>
2017-02-27 22:45:17 -0600

    The endpoint test does slightly more?

--- monolith/src/integration/groovy/com/insanedev/greetings/GreetingEndpointTest.groovy
+++ monolith/src/integration/groovy/com/insanedev/greetings/GreetingEndpointTest.groovy
@@ -1,6 +1,8 @@
 package com.insanedev.greetings
 
+import com.insanedev.greetings.templates.TemplateService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
@@ -15,6 +17,23 @@ class GreetingEndpointTest extends Specification {
     @Autowired
     TestRestTemplate restTemplate
 
+    @Autowired
+    TemplateService templateService
+
+    @Value('${pingBaseUrl}')
+    String pingBaseUrl
+
+    @Value('${greetingsBaseUrl}')
+    String greetingsBaseUrl
+
+    def setup() {
+        templateService.addTemplate("test", "hello world")
+    }
+
+    def teardown() {
+        templateService.truncateTemplates()
+    }
+
     def "context starts with a port"() {
         expect:
         port != 0
@@ -22,9 +41,17 @@ class GreetingEndpointTest extends Specification {
 
     def "ping responds"() {
         when:
-        String content = restTemplate.getForObject("/api/ping", String)
+        String content = restTemplate.getForObject(pingBaseUrl, String)
 
         then:
         content == "pong"
+    }
+
+    def "transient greeting endpoint responds"() {
+        when:
+        GreetingResponse response = restTemplate.getForObject("$greetingsBaseUrl/transient?template=test", GreetingResponse)
+
+        then:
+        response.greeting.greeting == "hello world"
     }
 }
